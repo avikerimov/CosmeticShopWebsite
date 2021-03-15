2b930439a476bddc23141f428f2bc86aa56c04d9
avikerimov <avikerimov1995@gmail.com>
2021-03-15 14:47:28 +0200

    try1

--- src/components/Man_Perfumes/DiorMan.jsx
+++ src/components/Man_Perfumes/DiorMan.jsx
@@ -3,7 +3,7 @@ import { Dior_Man_Carousel } from "../../services/Man_Perfumes/Dior_Man_Carousel
 import { Dior_Man_Perfumes } from "../../services/Man_Perfumes/Dior_Man_Perfumes";
 import Card from "../Card";
 import Carousel from "../Carousel";
-import logo from "../../Pictures/Man_Purfumes/Dior/DiorLogo.png"
+import logo from "../../Pictures/Man_Purfumes/Dior/DiorLogo.png";
 
 function DiorMan() {
   const [diorManPerfumes, setDiorManPerfumes] = useState([]);
@@ -18,13 +18,14 @@ function DiorMan() {
     <React.Fragment>
       <div className="container text-center mb-5">
         <h1 className="d-none">Dior Man</h1>
-        <img className="my-5" src={logo} alt=""/>
+        <img className="my-5" src={logo} alt="" />
         <Carousel carousel={diorManCarousel} />
+        <h2>Hello</h2>
       </div>
       <div className="row">
         {diorManPerfumes.map((dior, i) => (
           <div key={i} className="col-xl-3 col-lg-4 col-md-6 col-12 mb-3">
-            <Card card={dior} />            
+            <Card card={dior} />
           </div>
         ))}
       </div>
